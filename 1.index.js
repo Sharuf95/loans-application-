45e2a202dda732f9bcf74526b371f2d3dda4ca92
Sharuf shaik <shaiksharuf95@gmail.com>
2023-02-28 17:08:00 +0530

    loan application

---
+++ 1.index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const bodyParser = require ("body-parser");
+
+// Creating an express application 
+const app = express ();
+
+// handling json body request 
+app.use(bodyParser.json());
+
+app.get ("/",function(request,response) {
+response.json({
+status  : true,
+message : "Loans API running successfull"
+});                  
+});
+
+// post API for new loan application 
+
+app.post("/new-loan",function(request,response) {
+const loandata = request.body
+
+/*
+ const firstName = loandata.firstName;
+ const lastName  = loandata.lastName;
+ const email     = loandata.email;
+ const amount    = loandata.amount;
+ const purpose   = loandata.purpose*/
+ 
+ // You can do same thing using destructring object ( we can do like this also)
+
+ const {firstName,lastName,email,amount,purpose} = loandata;
+
+if(!firstName) {
+// return response.status(400).json({
+// status : false,
+// error : `firstName is required`
+// })
+return SendErrorResponse(response,`firstName is required`)
+};
+
+if(!lastName) {
+// return response.status(400).json({
+//status : false,
+//error : `lastName is required`
+// })
+return SendErrorResponse(response,`lastName is required`)
+};
+
+if(!email){
+//return response.status(400).json({
+//status : false,
+//error : `Email address is required`
+// })
+return SendErrorResponse(response,`Email address is required`)
+};
+
+
+if(!amount){
+// return response.status(400).json({
+//status : false,
+//error : `Please provide loan amount`
+//}) 
+return  SendErrorResponse(response,`Please provide loan amount`)
+};
+
+if(!purpose){
+//return response.status(400).json({
+// status : false,
+// error : `Purpose for loan is required`
+// })
+return SendErrorResponse(response,`Please provide loan amount`)
+}
+
+
+return response.json({
+status  : true,
+message : "New loan application created....",
+data    : loandata
+})
+})
+
+
+function SendErrorResponse(response,ErrorMessage) {
+return response.status(400).json({
+status : false,
+error  : ErrorMessage
+});
+}
+
+
+app.listen(4001,function(request,response) {
+console.log(`Server is running on  http://localhost:4001`)
+}) 
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # loans-application-
+
+## Project: Loan Applications DB
+
+Project Code : https://github.com/Sharuf95/loans-application
+
+Here is a sample data you can create in `loans` tables
+
+| ID | First Name | Last Name | Email | Loan Amount | Purpose | Status |
+|----|-----------|----------|-------|------------|---------|-------|
+| 1  | John      | Doe      | john.doe@example.com | 10000 | Home Improvement | Approved |
+| 2  | Jane      | Smith    | jane.smith@example.com | 5000 | Debt Consolidation | Approved |
+| 3  | Bob       | Johnson  | bob.johnson@example.com | 15000 | Business | Pending |
+| 4  | Emily     | Brown    | emily.brown@example.com | 8000 | Vacation | Approved |
+| 5  | Michael   | Davis    | michael.davis@example.com | 20000 | Education | Denied |
+| 6  | Sarah     | Wilson   | sarah.wilson@example.com | 12000 | Car Purchase | Approved |
+
+
+## Task 
+1. [ ] `CREATE` a table with `loans` name and define table columns as provide in above table in database
+1. [ ] create `INSERT` script for adding above provide data into 
+1. [ ] `SELECT` all loan applications from the table.
+1. [ ] `SELECT` all loan applications with a status of "Approved".
+1. [ ] `SELECT` all loan applications with a loan amount greater than 10000.
+1. [ ] `UPDATE` the status of all loan applications with a purpose of "Debt Consolidation" to "Approved".
+1. [ ] `UPDATE` the loan amount of a specific loan application by its ID.
+1. [ ] `DELETE` all loan applications with a status of "Denied".
+1. [ ] `SELECT` the first name, last name, and loan amount of all loan applications.
+1. [ ] `SELECT` the average loan amount of all approved loan applications.
+1. [ ] `UPDATE` the email address of a specific loan application by its ID.
+1. [ ] `DELETE` a specific loan application by its ID.
